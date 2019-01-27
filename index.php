42ff3c8a20166c4a69cd4eaf7a90fe729a7e3154
jkovacevic992 <jkovacevic992@gmail.com>
2019-01-27 12:47:26 +0100

    Promjena podataka postojećem Zaposleniku now works.

--- index.php
+++ index.php
@@ -21,13 +21,13 @@ while ($bool) {
         case 1:
             {
                 echo "*************************************************\n";
-                for($i=0;$i<count($zaposleniciArray);$i++){
-                    echo "ID: ".$zaposleniciArray[$i]->id;
-                    echo "IME: ".$zaposleniciArray[$i]->ime;
-                    echo "PREZIME: ".$zaposleniciArray[$i]->prezime;
-                    echo "DATUM ROĐENJA: ".$zaposleniciArray[$i]->datumRodenja;
-                    echo "SPOL: ".$zaposleniciArray[$i]->spol;
-                    echo "MJESEČNA PRIMANJA: ".$zaposleniciArray[$i]->mjesecnaPrimanja;
+                for ($i = 0; $i < count($zaposleniciArray); $i++) {
+                    echo "ID: " . $zaposleniciArray[$i]->id;
+                    echo "IME: " . $zaposleniciArray[$i]->ime;
+                    echo "PREZIME: " . $zaposleniciArray[$i]->prezime;
+                    echo "DATUM ROĐENJA: " . $zaposleniciArray[$i]->datumRodenja;
+                    echo "SPOL: " . $zaposleniciArray[$i]->spol;
+                    echo "MJESEČNA PRIMANJA: " . $zaposleniciArray[$i]->mjesecnaPrimanja;
                     echo "*************************************************\n";
                 }
                 echo "Želite li se vratiti na izbornik? (DA/NE)\n";
@@ -46,17 +46,22 @@ while ($bool) {
             }
         case 3:
             {
+                echo "Upišite ID zaposlenika čije podatke želite mijenjati\n";
+                $temp = fgets(STDIN);
+                $zaposleniciArray= promjenaZaposlenika($zaposleniciArray, $temp);
 
                 break;
             }
         case 4:
             {
                 echo "Upišite ID zaposlenika kojeg želite obrisati: \n";
-                $temp=fgets(STDIN);
+                $temp = fgets(STDIN);
                 echo "Jeste li sigurni da želite obrisati polaznika s ID-em $temp? (DA/NE)\n";
 
-                if(readline() !== 'da'){
+                if (readline() !== 'da') {
                     echo "Zaposlenik nije obrisan.\n";
+                }else{
+                    $zaposleniciArray= brisanjeZaposlenika($zaposleniciArray,$temp);
                 }
 
                 break;
@@ -91,7 +96,8 @@ function ispisiIzbornik()
     echo "*************************************************\n";
 }
 
-function unosZaposlenika(){
+function unosZaposlenika()
+{
 
     echo "ID: ";
     $id = fgets(STDIN);
@@ -109,17 +115,86 @@ function unosZaposlenika(){
 
 }
 
-//
-//function brisanjeZaposlenika($array,$zaposlenikId){
-//    for($i=0;$i<=count($array);$i++){
-//        if(isset($array[$i]) && $array[$i]->getId()===$zaposlenikId){
-//            $array[$i]= null;
-//            unset($array[$i]);
-//        }
-//    }
-//    $array = array_values($array);
-//    return $array;
-//}
+
+function brisanjeZaposlenika($array,$zaposlenikId)
+{
+    for($i=0;$i<=count($array);$i++){
+        if(isset($array[$i]) && $array[$i]->id===$zaposlenikId){
+            $array[$i]= null;
+            unset($array[$i]);
+
+        }
+    }
+    $array = array_values($array);
+    return $array;
+}
+
+function promjenaZaposlenika($array,$zaposlenikId)
+{
+
+    for ($i = 0; $i <= count($array); $i++) {
+        if ($array[$i]->id !== $zaposlenikId) {
+            echo "Zaposlenik s tim ID-em ne postoji.";
+            break;
+
+
+        } else {
+            echo "Koji podatak želite promijeniti (1-6)?\n";
+            echo "1. ID\n";
+            echo "2. IME\n";
+            echo "3. PREZIME\n";
+            echo "4. DATUM ROĐENJA\n";
+            echo "5. SPOL\n";
+            echo "6. MJESEČNA PRIMANJA\n";
+            switch (fgets(STDIN)) {
+                case 1:
+                    {
+                        echo "Stara vrijednost ID-a je ". $array[$i]->id . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->id= fgets(STDIN);
+                    break;
+                }
+                case 2:
+                    {
+                        echo "Staro ime je ". $array[$i]->ime . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->ime= fgets(STDIN);
+                        break;
+                    }
+                case 3:
+                    {
+                        echo "Staro prezime je ". $array[$i]->prezime . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->prezime= fgets(STDIN);
+                        break;
+                    }
+                case 4:
+                    {
+                        echo "Stari datum rođenja je ". $array[$i]->datumRodenja . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->datumRodenja= fgets(STDIN);
+                        break;
+
+                    }
+                case 5:
+                    {
+                        echo "Stari spol je ". $array[$i]->spol . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->spol= fgets(STDIN);
+                        break;
+                    }
+                case 6:
+                    {
+                        echo "Stari iznos mjesečnih primanja je  ". $array[$i]->mjesecnaPrimanja . "\n";
+                        echo "Unesite novu vrijednost:\n";
+                        $array[$i]->mjesecnaPrimanja= fgets(STDIN);
+
+                    }
+            }
+        }
+    }
+    return $array;
+}
 
 
 
